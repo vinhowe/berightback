bcdadf7457a2050627e769be46d323c299f2e3ae
Vin Howe <24789592+iiridescent@users.noreply.github.com>
2020-02-18 11:53:19 -0700

    Hacky nuclear option for halfway-addressing #1

--- fullscreen_lock.sh
+++ fullscreen_lock.sh
@@ -1,14 +1,15 @@
+#! /usr/bin/env bash
 MSG=${1:-"I'll be right back!"}
 PAGE_PORT=${2:-8080}
 
-cd ~/dev/webcam_shower/ && source venv/bin/activate
-python berightback.py -m "$MSG" &
+source ./venv/bin/activate && python3 berightback.py -m "$MSG" &
 firefox -new-instance -P "webcam" --kiosk "http://0.0.0.0:$PAGE_PORT" &
 sleep .5 && xdotool search --sync --onlyvisible --name "$MSG" windowactivate key F11
 xtrlock &
 LOCK_PID=$!
 wait $LOCK_PID
 pkill -P $$
-# kill -9 $WC_PID
+pgrep -f -a berightback
+pkill motion
 reset
 clear
